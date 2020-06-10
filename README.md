240448ce8156a380fcf0fb907ff30c002fa95bcd
johnmcarter <jmcarter211@gmail.com>
2020-06-09 20:42:55 -0400

    it seems to work now

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ pip3 install -r requirements.txt
 ```
 in the same directory on the command line. To then run the code, run
 ```
-python3 neural_network_art.py
+python3 neural_network_art.py <style image> <content image> <style weight (optional)> <content weight (optional)> <output (optional)>
 ```
-in the local directory on the command line. 
+in the local directory on the command line. There is a default value of 1e5 for style weight, 1 for content weight, and the program saves the output to images/output.png by default. To create the output.png file with the provided crucifixion.jpg style image and plaza_de_espana.jpg content image, run
+```
+python3 neural_network_art.py crucifixion.jpg plaza_de_espana.jpg
+```
--- neural_network_art.py
+++ neural_network_art.py
@@ -13,6 +13,9 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torchvision.transforms as transforms
 import torchvision.models as models
+from torchvision.utils import save_image
+
+import argparse
 
 # Check if GPU is available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -25,21 +28,14 @@ loader = transforms.Compose([transforms.Resize(IMG_SIZE),
                             transforms.ToTensor()])
 
 def load_image(name):
+    '''
+    Load an image to transform it into a tensor and return it
+    @param name: image name to open
+    '''
     im = Image.open(name)
     im = loader(im).unsqueeze(0)
     
     return im.to(device, torch.float)
-
-# Style image from https://www.pablopicasso.org/crucifixion.jsp
-style = load_image("images/crucifixion.jpg")
-content = load_image("images/plaza_de_espana.jpg")
-
-# Check to make sure content and style are the same size. Exit if they aren't
-if style.size() != content.size():
-    print("Style and content photos must be same size!")
-    exit()
-
-plt.ion()
 
 def show_img(tensor, name=None):
     '''
@@ -52,73 +48,264 @@ def show_img(tensor, name=None):
         plt.title(name)
     plt.pause(1)
 
-plt.figure()
-show_img(style, name="Style image")
-plt.figure()
-show_img(content, name="Content image")
-
-# import the pre-trained CNN
-cnn = models.vgg19(pretrained=True).features.to(device).eval()
-
-content_layers = ['conv_4']
-style_layers = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
-
-mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
-std = torch.tensor([0.229, 0.224, 0.225]).to(device)
-
-
-def compute_gram(input):
+def compute_sigma(input):
+    '''
+    Compute the sigma matrix by multiplying the feature map and
+    and the transposed feature map
+    '''
     dim_1, dim_2, dim_3, dim_4 = input.size()
     feat_map = input.reshape(dim_1 * dim_2, dim_3 * dim_4)
 
-    # Compute gram matrix as dot product of feat_map and feat_map transposed
-    gram_matrix = torch.mm(feat_map, feat_map.t())
-    #Normalize gram matrix by dividing by dimension sizes
-    gram_matrix = gram_matrix.div(dim_1 * dim_2 * dim_3 * dim_4)
+    # Compute sigma as dot product of feat_map and feat_map transposed
+    sigma = torch.mm(feat_map, feat_map.t())
+    # Normalize sigma by dividing by dimension sizes
+    sigma = sigma.div(dim_1 * dim_2 * dim_3 * dim_4)
 
-    return gram_matrix
+    return sigma
 
 class Normalize(nn.Module):
+    '''
+    Normalize the model by subtracting the mean and dividing by
+    the standard deviation
+    '''
     def __init__(self, mean, std):
+        '''
+        @param mean: means for each dimension
+        @param std: std's for each dimension
+        '''
         super(Normalize, self).__init__()
         # Make dimensions of mean and std compatible, so -1 for the number of channels
         # in the image and 1's for the height and width of the image
-        self.mean = torch.tensor(mean).reshape(-1, 1, 1)
-        self.std = torch.tensor(std).reshape(-1, 1, 1)
+        self.mean = torch.as_tensor(mean).reshape(-1, 1, 1)
+        self.std = torch.as_tensor(std).reshape(-1, 1, 1)
 
     def forward(self, img):
-        #Normalize image
+        '''
+        Normalize param 'img'
+        '''
         return (img - self.mean)/self.std
 
-class ContentMse(nn.Module):
+class ContentMSE(nn.Module):
+    '''
+    Get the mean squared error loss for the content image
+    '''
     def __init__(self, target,):
-        super(ContentMse, self).__init__()
+        super(ContentMSE, self).__init__()
         self.target = target.detach()
     
     def forward(self, input):
         self.loss = F.mse_loss(input, self.target)
         return input
 
-class StyleMse(nn.Module):
+class StyleMSE(nn.Module):
+    '''
+    Get the mean squared error loss for the style painting
+    '''
     def __init__(self, target_feat):
-        super(StyleMse, self).__init__()
-        self.target = compute_gram(target_feat).detach()
+        super(StyleMSE, self).__init__()
+        self.target = compute_sigma(target_feat).detach()
     
     def forward(self, input):
-        gram_matrix = compute_gram(input)
-        self.loss = F.mse_loss(gram_matrix, target)
+        '''
+        Compute the sigma matrix, and then compute the loss between it
+        and the target using mean squared error
+        '''
+        sigma = compute_sigma(input)
+        self.loss = F.mse_loss(sigma, self.target)
         return input
 
-def get_loss_modules(cnn, style_img, content_img, mean, std, 
-                    content_layers = content_layers, style_layers = style_layers):
+def get_model_and_losses(cnn, style, content, mean, std, 
+                    content_layers, style_layers):
+    '''
+    Get the CNN and the losses for the style and content images to be used
+    in the run() function.
+    @param cnn: the imported pre-trained CNN
+    @param style: input style painting image
+    @param content: input content image
+    @param mean: initial means for each dimension
+    @param std: initial std's for each dimension
+    @param content_layers: depth layers to compute content losses
+    @param style_layers: depth layers to compute style losses
+
+    @return model: the CNN model we made
+    @return style_losses: list of losses for style painting image
+    @return content_losses: list of losses for content image
+    '''
     
     cnn = deepcopy(cnn)
-    content_loss, style_loss = [], []
+    content_losses, style_losses = [], []
 
     normalized = Normalize(mean, std).to(device)
-
     model = nn.Sequential(normalized)
 
-    print(model)
+    conv_count = 0
 
-get_loss_modules(cnn, style, content, mean, std)
+    for layer in cnn.children():
+        # Check if it's a convolution layer
+        if isinstance(layer, nn.Conv2d):
+            conv_count += 1
+            layer_name = 'conv_{}'.format(conv_count)
+        # Check if it's an activation layer
+        elif isinstance(layer, nn.ReLU):
+            layer_name = 'relu_{}'.format(conv_count)
+            layer = nn.ReLU(inplace=False)
+        # Check if it's a pooling layer
+        elif isinstance(layer, nn.MaxPool2d):
+            layer_name = 'pool_{}'.format(conv_count)
+        # Check if it's a batch normalization layer
+        elif isinstance(layer, nn.BatchNorm2d):
+            layer_name = 'bn_{}'.format(conv_count)
+        else:
+            print('\033[91m Unrecognized layer: {} \033[0m'\
+                                .format(layer.__class__.__name__))
+
+        model.add_module(layer_name, layer)
+
+        # Check if we're in one of the style layers
+        if layer_name in style_layers:
+            target_feat = model(style).detach()
+            loss = StyleMSE(target_feat)
+            model.add_module("style_loss_{}".format(conv_count), loss)
+            style_losses.append(loss)
+
+        # Check if we're in one of the content layers
+        if layer_name in content_layers:
+            target_feat = model(content).detach()
+            loss = ContentMSE(target_feat)
+            model.add_module("style_loss_{}".format(conv_count), loss)
+            content_losses.append(loss)
+
+    for i in range(len(model)-1, -1, -1):
+        if isinstance(model[i], ContentMSE) or isinstance(model[i], StyleMSE):
+            break
+
+    model = model[:(i+1)]
+
+    return model, style_losses, content_losses
+
+
+def run(cnn, mean, std, content, style, image, \
+        content_layers, style_layers, style_weight, content_weight, epochs=300):
+    '''
+    Perform the style transfer. Run epochs times and build the mixture
+    of content and style according to the style_weight, and content_weight.
+    @param cnn: the imported pre-trained CNN
+    @param mean: initial means for each dimension
+    @param std: initial std's for each dimension
+    @param content: input content image
+    @param style: input style painting image
+    @param input_image: initially a clone of content
+    @param epochs: number of times to run
+    @param style_weight: amount of influence to give style painting relative
+    to the content
+    @param content_weight: amount of influence to give content image relative
+    to the style
+
+    @return image: the final image
+    '''
+    print("\u001b[1m\033[92mStarting the content/style transfer...\033[0m")
+    # Get the model and losses
+    model, style_losses, content_losses = \
+        get_model_and_losses(cnn, style, content, mean, std, \
+                                content_layers, style_layers)
+
+    # Optimize with LBFGS
+    optimizer = optim.LBFGS([image.requires_grad_()])
+
+    run = [0]
+    while run[0] <= epochs:
+        # Define as closure() because it's required by LBFGS
+        def closure():
+            image.data.clamp_(0,1)
+            optimizer.zero_grad()
+            model(image)
+            s_score = 0
+            c_score = 0
+
+            # Populate all the losses from both content and style
+            for loss in content_losses:
+                c_score += loss.loss
+            for loss in style_losses:
+                s_score += loss.loss
+
+            # Scale the scores by the weights passed into the function
+            c_score *= content_weight
+            s_score *= style_weight
+
+            total_loss = c_score + s_score
+            total_loss.backward(retain_graph=True)
+
+            run[0] += 1
+
+            # Print current stats
+            if run[0] % 20 == 0:
+                print("\u001b[35mrun iteration {}".format(run))
+                print("\u001b[33mstyle loss: {:4f}".format(s_score.item()))
+                print("content loss: {:4f}".format(c_score.item()))
+                print("\u001b[0m")
+
+            return c_score + s_score
+            
+        optimizer.step(closure)
+
+    image.data.clamp_(0,1)
+
+    return image
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Blends a content image and style painting')
+    parser.add_argument('style', type=str, help='Style image')
+    parser.add_argument('content', type=str, help='Content image')
+    parser.add_argument('--sw', type=int, default=1e5,
+                        help='Style weight (int)')
+    parser.add_argument('--cw', type=int, default=1,
+                        help='Content weight (int)')
+    parser.add_argument('--output', type=str, default='images/output.png',
+                        help='Place to save the output')
+    args = parser.parse_args()
+
+    style = load_image("images/{}".format(args.style))
+    content = load_image("images/{}".format(args.content))
+    input_image = content.clone()
+
+    # Check to make sure content and style are the same size. Exit if they aren't
+    if style.size() != content.size():
+        print("\u001b[31mStyle and content photos must be same size!\u001b[0m")
+        exit()
+
+    plt.ion()
+
+    # Import the pre-trained CNN
+    cnn = models.vgg19(pretrained=True).features.to(device).eval()
+
+    # Define the style and content layers we'll use in our CNN
+    content_layers = ['conv_4']
+    style_layers = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+
+    # Set initial mean and standard deviation
+    mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
+    std = torch.tensor([0.229, 0.224, 0.225]).to(device)
+
+    print("\u001b[35m")
+    print("Style image:", args.style)
+    print("Content image:", args.content)
+    print("Style weight:", args.sw)
+    print("Content weight:", args.cw)
+    print("\u001b[0m")
+
+    # Call run() and display the output image
+    output_image = run(cnn, mean, std, content, \
+            style, input_image, content_layers, \
+            style_layers, args.sw, args.cw, epochs=300)
+
+    plt.figure()
+    show_img(output_image, name='Output image')
+    plt.ioff()
+    plt.show()
+
+    # Save the output image in the current directory
+    print("\u001b[1m\033[92mSaving output image in the images directory\033[0m")
+    final_img = output_image[0]
+    save_image(final_img, 'images/output.png')
+
